e6fafd982a05e03789112dc7f1c58ae14b31de33
riteshtekchandani18 <riteshtekchandani0@gmail.com>
2023-12-26 18:37:21 +0530

    form

--- shop/urls.py
+++ shop/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
  #listing of products
     path('products/all/', views.all_products, name='all_products'),
-    path('products/<slug:brands>/all/',views.brand_products,name='brand_products'),
-    path('products/<slug:category>/all/',views.category_products,name='category_products'),
-    path('products/<slug:brands>/<slug:category>/all/',views.brands_category_products,name='brand_category_products'),
+    path('brands/<slug:brand>/all/',views.brandproduct,name='brandproduct'),
+    path('category/<slug:category>/all/',views.category_products,name='category_products'),
+   
 
     #seaching products
     path('products/search/',views.search_products, name='search_products'),
--- shop/views.py
+++ shop/views.py
@@ -1,23 +1,47 @@
 from django.shortcuts import render
 from .models import Product,Brand,Category
 from .filters import ProductFilter
+from  django.contrib import messages
+
 def all_products(request):
     f = ProductFilter(request.GET, queryset=Product.objects.all())
-    print(f.get_filters())
     return render(request,'shop/products.html',{
         'filter':f,
         'brands':Brand.objects.filter(favorite=True),
         'categories':Category.objects.all()
     })
 
-def brand_products(request,brand):
-    pass
+def brandproduct(request,brand):
+     brandObj = Brand.objects.get(slug=brand)
+     products=Product.objects.filter(brand=brandObj)
+     return render(request,'shop/brandproduct.html',{
+        'brand': brandObj,
+        'products': products,
+        'brands': Brand.objects.all(),
+        'categories': Category.objects.all()
+    })
 
 def category_products(request,category):
-    pass
-
-def brands_category_products(request,brand,category):
-    pass
-
+     catObj = Category.objects.get(slug=category)
+     products=Product.objects.filter(category=catObj)
+     return render(request,'shop/category_products.html',{
+        'category': catObj,
+        'products': products,
+        'brands': Brand.objects.all(),
+        'categories': Category.objects.all()
+        })
 def search_products(request):
-    pass
+     q = request.GET.get('q')
+     if q is None:
+          messages.error(request, 'Please enter something to search!')
+     product_list_1 = Product.objects.filter(name__icontains=q)
+     product_list_2 = Product.objects.filter(brand__name__icontains =q)
+     product_list_3 = Product.objects.filter(category__name__icontains =q)
+     return render(request,'shop/search.html',{
+        'products': product_list_1.union(product_list_2,product_list_3),
+        'brands': Brand.objects.filter(name__icontains=q),
+        'categories':Category.objects.filter(name__icontains=q),
+        'q':q,
+        'total_items': product_list_1.count() + product_list_2.count() + product_list_3.count()
+     })
+  
